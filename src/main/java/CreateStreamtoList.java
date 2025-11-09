8280ed05c35aa1922191ec9d9b9f727983102fb2
Subbu <vsubbareddy2022@gmail.com>
2025-11-09 22:56:41 +0530

    Streams

--- src/main/java/CreateStreamtoList.java
+++ src/main/java/CreateStreamtoList.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -7,6 +9,21 @@ public class CreateStreamtoList {
         Stream<String> stream = Stream.of("Apple", "Banana", "Cherry", "Date");
 
         List<String> list = stream.collect(Collectors.toList());
-        System.out.println(list);
+        System.out.println("Converting Stream to List: " + list);
+
+        Stream<String> stream1 = Stream.of("A", "B", "C", "A");
+        Set<String> set = stream1.collect(Collectors.toSet());
+        System.out.println("Converting Stream to Set: " + set);
+
+        Stream<String> stream2 = Stream.of("A", "B", "C", "A");
+        String[] array = stream2.toArray(String[]::new);
+        System.out.println("Converting Stream to Array: " + Arrays.toString(array));
+
+        List<String> list1 = Arrays.asList("X", "Y", "Z");
+        List<String> list2 = Arrays.asList("1", "2", "3");
+
+        List<String> mergedList = Stream.concat(list1.stream(), list2.stream())
+                .collect(Collectors.toList());
+        System.out.println("Merging two Lists using Stream: " + mergedList);
     }
 }
